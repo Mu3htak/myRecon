055825238f77e0eca6b6508417b541507dc17f0c
Mushtak Hussain <34830693+Mu3htak@users.noreply.github.com>
2021-12-03 22:48:33 +0530

    Add files via upload

---
+++ myrecon.sh
@@ -0,0 +1,38 @@
+usage() {
+    echo -e "Usage: ./myrecon.sh -d domain.com" 1>&2;
+    exit 1;
+}
+
+while getopts ":d:" o; do
+    case "${o}" in
+        d)
+            domain=${OPTARG} ;;
+
+        *)
+            usage ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+echo "> PLEASE WAIT WHILE WE AUTOMATE YOUR LITTLE PIECE OF SHIT..."
+
+#Sublist3r enumeration
+printf "[+] Running sublist3r\n"
+python3 /home/mu3ht4k/opt/Sublist3r/sublist3r.py -d $domain -o domains.txt > /dev/null
+
+#Assetfinder enumeration
+printf "[+] Running assetfinder\n"
+assetfinder -subs-only $domain >> domains.txt
+
+#Removing duplicate entries
+printf "[+] Removing Duplicates from domains.txt\n"
+sort -u domains.txt > final_domains.txt
+
+printf "[+] Removed Duplicates and Saved as final_domains.txt\n"
+
+#Checking for alive domains using httpx
+printf "[+] Checking for alive domain\n"
+cat final_domains.txt | httpx -silent -mc 200 > alive.txt
+
+printf "[+] Saved alive domains in alive.txt\n"
+printf "DONEEEE!!!!!!\n"
